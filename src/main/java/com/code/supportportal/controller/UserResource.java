9ac4f3bfd321333dd3f193f89c9c380330beb773
Cruz Badillo <cubj25@gmail.com>
2021-02-23 17:05:48 -0600

    Create custom exception for User class

--- src/main/java/com/code/supportportal/controller/UserResource.java
+++ src/main/java/com/code/supportportal/controller/UserResource.java
@@ -1,4 +1,4 @@
-package com.code.supportportal.resources;
+package com.code.supportportal.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
