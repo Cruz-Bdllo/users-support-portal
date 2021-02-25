5497d17315653af4f7b3b5a1a363b1e4ac14ec4e
Cruz Badillo <cubj25@gmail.com>
2021-02-24 20:16:38 -0600

    Class for create memory cache for save the attempts login (using guava)

---
+++ src/main/java/com/code/supportportal/service/login/LoginAttemptService.java
@@ -0,0 +1,47 @@
+package com.code.supportportal.service.login;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
+
+@Service
+public class LoginAttemptService {
+    public static final int MAXIMUM_NUMBER_OF_ATTEMPTS = 5;
+    public static final int ATTEMPT_INCREMENT = 1;
+    public LoadingCache<String, Integer> attemptCache;
+
+    public LoginAttemptService() {
+        attemptCache = CacheBuilder.newBuilder()
+                .maximumSize(100)
+                .expireAfterWrite(15, MINUTES)
+                .build(
+                        new CacheLoader<String, Integer>() {
+                            @Override
+                            public Integer load(String username) throws Exception {
+                                return 0;
+                            }
+                        }
+                );
+    } // end method
+
+    public void deleteUserFromCache(String username) {
+        attemptCache.invalidate(username);
+    } // end method
+
+    public void addUserToCache(String username) throws ExecutionException {
+        int attempts = 0;
+        attempts = attemptCache.get(username) + ATTEMPT_INCREMENT;
+        attemptCache.put(username, attempts);
+    }
+
+    public boolean hasExceedMaxAttempts(String username) throws ExecutionException {
+        return attemptCache.get(username) >= MAXIMUM_NUMBER_OF_ATTEMPTS;
+    }
+
+} // end service
